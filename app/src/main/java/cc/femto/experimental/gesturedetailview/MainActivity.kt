34e2d9c9f6568d60ce2c17734ad09b464aa5a900
Henning Post <henningpost@gmail.com>
2019-05-03 09:49:25 -0700

    Initial commit

---
+++ app/src/main/java/cc/femto/experimental/gesturedetailview/MainActivity.kt
@@ -0,0 +1,17 @@
+package cc.femto.experimental.gesturedetailview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val card = findViewById<MaterialCardView>(R.id.card)
+//        val fab = findViewById<FloatingActionButton>(R.id.fab)
+    }
+}
